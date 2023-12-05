ea4b36e6ade8fc233c6ebc2953b09f3199f993c1
Miguel <miguelcarrizo276@gmail.com>
2023-12-05 17:59:07 -0300

    cambios

--- Mutual/src/Entidades/Prestador.java
+++ Mutual/src/Entidades/Prestador.java
@@ -1,27 +1,20 @@
 package Entidades;
 
 public class Prestador extends Persona {
-    private Plan plan;
+    private Especialidad especialidad;
 
-    public Prestador(Plan plan, int id, String nombre, String apellido, String direccion, int dni, int telefono, boolean estado) {
+    public Prestador(Especialidad especialidad, int id, String nombre, String apellido, String direccion, int dni, int telefono, boolean estado) {
         super(id, nombre, apellido, direccion, dni, telefono, estado);
-        this.plan = plan;
+        this.especialidad = especialidad;
     }
- 
-    public Prestador(Plan plan, String nombre, String apellido, String direccion, int dni, int telefono, boolean estado) {
+
+    public Prestador(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Prestador(Especialidad especialidad, String nombre, String apellido, String direccion, int dni, int telefono, boolean estado) {
         super(nombre, apellido, direccion, dni, telefono, estado);
-        this.plan = plan;
-    }
-
-    public Prestador() {
-    }
-
-    public Plan getPlan() {
-        return plan;
-    }
-
-    public void setPlan(Plan plan) {
-        this.plan = plan;
+        this.especialidad = especialidad;
     }
     
 
--- Mutual/src/Main/Main.java
+++ Mutual/src/Main/Main.java
@@ -3,7 +3,9 @@ package Main;
 public class Main {
     public static void main(String[] args) {
        
+        int edad = 10;
         
+        System.out.println(edad);
     }
     
   
